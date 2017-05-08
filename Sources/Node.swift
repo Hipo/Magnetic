8fcb279f8412823b1d0623214b061242b7a29a08
Mert Ahmet Güneş <mertgunes1905@gmail.com>
2017-05-08 15:16:52 +0300

    Left color blend factor control to the developer

--- Sources/Node.swift
+++ Sources/Node.swift
@@ -33,7 +33,6 @@ open class Node: SKShapeNode {
     public lazy var sprite: SKSpriteNode = { [unowned self] in
         let sprite = SKSpriteNode()
         sprite.size = self.frame.size
-        sprite.colorBlendFactor = 0.1
         self.mask.addChild(sprite)
         return sprite
     }()
